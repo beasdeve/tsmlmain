5cc6910fbbdad4c7627ce8d7c73748077eefc5d1
Sikandar Bhuimali <sikandar.bhulmali@beas.co.in>
2023-05-24 15:28:08 +0530

    changes api

--- routes/api.php
+++ routes/api.php
@@ -73,7 +73,7 @@ use App\Http\Controllers\Api\Modules\Admin\LedgerController;
 |
 */
 // Route::post('login', [UserController::class, 'login']);
-// chk
+// chk1
 // ----------------------------------- register -----------------------------------------
 
 Route::post('/register', [UserController::class,'store']);
