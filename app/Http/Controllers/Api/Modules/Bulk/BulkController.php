c7a587235abe950022a118f7a6d865a8a7070ff8
souravbeas17 <sourav.mondal@beas.co.in>
2023-05-30 11:17:25 +0530

    bulk chngs

--- app/Http/Controllers/Api/Modules/Bulk/BulkController.php
+++ app/Http/Controllers/Api/Modules/Bulk/BulkController.php
@@ -21,10 +21,6 @@ class BulkController extends Controller
     {
         $response = [];
         try{
-
-            // ini_set('upload_max_filesize', '128M');
-            // ini_set( 'post_max_size', '128M');
-            // ini_set( 'max_execution_time', '300');
          if ($request->hasFile('excel'))
          {
             $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
@@ -38,44 +34,43 @@ class BulkController extends Controller
 
             foreach($sheetData as $val)
             {
-                 
-                if(!empty($val[14])  && !empty($val[13]))
+                if(!empty($val[18])  && !empty($val[15]))
                 {
 
-                $check_user = User::where('email',$val[14])->first();
-                $check_codet = Address::where('cus_code',$val[0])->first();
-                 
+                $check_user = User::where('email',$val[18])->first();
+                $check_codet = Address::where('cus_code',$val[1])->first();
+                // dd($check_user->user_code);
 
                 if(empty($check_user))
                 {                    
                      $password = 'Test@12345';
                 
                 
-                $check_email = User::where('name',strtoupper(trim($val[2])))->first();
+                $check_email = User::where('name',strtoupper(trim($val[3])))->first();
                 // return $check_email;exit();
                 if ($check_email=='') {
                 $user = new User;
-                $user->user_code = trim($val[0]); 
-                $user->name = strtoupper(trim($val[2]));
-                $user->email = $val[14];
-                $user->phone = $val[13];
-                $user->gstin = $val[30];
-                $user->org_pan = $val[32];
+                $user->user_code = trim($val[1]); 
+                $user->name = strtoupper(trim($val[3]));
+                $user->email = $val[18];
+                $user->phone = $val[15];
+                $user->gstin = $val[126];
+                $user->org_pan = $val[203];
                 $user->password = $password;
-                $user->org_name = strtoupper(trim($val[4]));
-                $user->org_address = $val[5];
+                $user->org_name = strtoupper(trim($val[5]));
+                $user->org_address = $val[6];
                 $user->user_type = 'C';
                 $user->login_attempt = 2;
-                $user->company_pan = $val[32];
-                $user->addressone = $val[12];
-                $user->zone = $val[9];
+                $user->company_pan = $val[200];
+                $user->addressone = $val[6];
+                $user->zone = $val[10];
 
                 $user->save();
                 }
                 
                 $check_gst_arr = array();
-                $check_gst = Address::where('cus_code',$val[0])->get();
-                $user_details = User::where('name',strtoupper(trim($val[2])))->first();
+                $check_gst = Address::where('cus_code',$val[1])->get();
+                $user_details = User::where('name',strtoupper(trim($val[3])))->first();
                 // return $user->id;exit();
                 foreach ($check_gst as $key => $value) {
                     array_push($check_gst_arr, $value->id);
@@ -87,23 +82,22 @@ class BulkController extends Controller
                 // billing-address
              // if($user->user_code == $val[1])
              // {
-                    $valb = strtolower($val[20]);
+                    $valb = strtolower($val[55]);
 
                     $billing = new Address;
                     $billing->user_id = $user_details->id;
-                    $billing->addressone = $val[12];
-                    $billing->addresstwo = $val[5];
+                    $billing->addressone = $val[6];
+                    $billing->addresstwo = $val[14];
                     $billing->city = ucwords($valb);
-                    $billing->state = $val[8];
-                    $billing->pincode = $val[7];
-                    $billing->cityc = $val[19];
+                    $billing->state = $val[9];
+                    $billing->pincode = $val[8];
+                    $billing->cityc = $val[54];
                     $billing->type = 'B';
-                    $billing->company_name = strtoupper(trim($val[4]));
-                    $billing->gstin =  $val[30];
-                    $billing->cus_code =  $val[0];
-                    $billing->country =  $val[1];
-                    $billing->cust_group_name = strtoupper(trim($val[2]));
-                    $billing->zone = $val[9];
+                    $billing->company_name = strtoupper(trim($val[5]));
+                    $billing->gstin =  $val[203];
+                    $billing->cus_code =  $val[1];
+                    $billing->country =  $val[2];
+                    $billing->cust_group_name = strtoupper(trim($val[3]));
                     $billing->save();
                 // }
                 
@@ -113,19 +107,18 @@ class BulkController extends Controller
                 // $vals = strtolower($val[55]);
                 $shipping = new Address;
                 $shipping->user_id = $user_details->id;
-                $shipping->addressone = $val[12];
-                $shipping->addresstwo = $val[5];
+                $shipping->addressone = $val[6];
+                $shipping->addresstwo = $val[14];
+                $shipping->cityc = $val[54];
                 $shipping->city = ucwords($valb);
-                $shipping->state = $val[8];
-                $shipping->pincode = $val[7];
-                $shipping->cityc = $val[19];
+                $shipping->state = $val[9];
+                $shipping->pincode = $val[8];
                 $shipping->type = 'A';
-                $shipping->company_name = strtoupper(trim($val[4]));
-                $shipping->gstin =  $val[30];
-                $shipping->cus_code =  $val[0];
-                $shipping->country =  $val[1];
-                $shipping->cust_group_name = strtoupper(trim($val[2]));
-                $shipping->zone = $val[9];
+                $shipping->company_name = strtoupper(trim($val[5]));
+                $shipping->gstin =  $val[203];
+                $shipping->cus_code =  $val[1];
+                $shipping->country =  $val[2];
+                $shipping->cust_group_name = strtoupper(trim($val[3]));
                 $shipping->save();
               // }
             }
@@ -142,8 +135,8 @@ class BulkController extends Controller
 
 
                     $check_gst_arr = array();
-                    $check_gst = Address::where('cus_code',$val[0])->get();
-                    $user_details = User::where('name',strtoupper(trim($val[2])))->first();
+                    $check_gst = Address::where('cus_code',$val[1])->get();
+                    $user_details = User::where('name',strtoupper(trim($val[3])))->first();
                 // return $user->id;exit();
                     foreach ($check_gst as $key => $value) {
                         array_push($check_gst_arr, $value->id);
@@ -159,21 +152,19 @@ class BulkController extends Controller
 
                             $billing = new Address;
                             $billing->user_id = $user_details->id;
-                            $billing->addressone = $val[12];
-                            $billing->addresstwo = $val[5];
+                            $billing->addressone = $val[6];
+                            $billing->addresstwo = $val[14];
                             $billing->city = ucwords($valb);
-                            $billing->state = $val[8];
-                            $billing->pincode = $val[7];
-                            $billing->cityc = $val[19];
+                            $billing->state = $val[9];
+                            $billing->pincode = $val[8];
+                            $billing->cityc = $val[54];
                             $billing->type = 'B';
-                            $billing->company_name = strtoupper(trim($val[4]));
-                            $billing->gstin =  $val[30];
-                            $billing->cus_code =  $val[0];
-                            $billing->country =  $val[1];
-                            $billing->cust_group_name = strtoupper(trim($val[2]));
-                            $billing->zone = $val[9];
+                            $billing->company_name = strtoupper(trim($val[5]));
+                            $billing->gstin =  $val[203];
+                            $billing->cus_code =  $val[1];
+                            $billing->country =  $val[2];
+                            $billing->cust_group_name = strtoupper(trim($val[3]));
                             $billing->save();
-
                         // }
                         
                         // if($user->user_code != $val[1])
@@ -182,19 +173,18 @@ class BulkController extends Controller
                         // $vals = strtolower($val[55]);
                         $shipping = new Address;
                         $shipping->user_id = $user_details->id;
-                        $shipping->addressone = $val[12];
-                        $shipping->addresstwo = $val[5];
+                        $shipping->addressone = $val[6];
+                        $shipping->addresstwo = $val[14];
+                        $shipping->cityc = $val[54];
                         $shipping->city = ucwords($valb);
-                        $shipping->state = $val[8];
-                        $shipping->pincode = $val[7];
-                        $shipping->cityc = $val[19];
+                        $shipping->state = $val[9];
+                        $shipping->pincode = $val[8];
                         $shipping->type = 'A';
-                        $shipping->company_name = strtoupper(trim($val[4]));
-                        $shipping->gstin =  $val[30];
-                        $shipping->cus_code =  $val[0];
-                        $shipping->country =  $val[1];
-                        $shipping->cust_group_name = strtoupper(trim($val[2]));
-                        $shipping->zone = $val[9];
+                        $shipping->company_name = strtoupper(trim($val[5]));
+                        $shipping->gstin =  $val[203];
+                        $shipping->cus_code =  $val[1];
+                        $shipping->country =  $val[2];
+                        $shipping->cust_group_name = strtoupper(trim($val[3]));
                         $shipping->save();
                       // }
                     }
@@ -205,27 +195,24 @@ class BulkController extends Controller
                   
 
                   $userUp['org_name'] = strtoupper(trim($val[5]));
-                  $userUp['org_pan'] = $val[32];
-                  $userUp['gstin'] = $val[30];
-                  $userUp['phone'] = $val[13];
+                  $userUp['org_pan'] = $val[203];
+                  $userUp['gstin'] = $val[126];
+                  $userUp['phone'] = $val[15];
 
                   User::where('id',$check_user->id)->update($userUp);
                   
-                  $valb = strtolower($val[20]);
+                  $valb = strtolower($val[55]);
 
-                $asddUp['addressone'] = $val[12];
-                $asddUp['addresstwo'] = $val[5];
-                $asddUp['cityc'] = $val[19];
+                $asddUp['addressone'] = $val[6];
+                $asddUp['addresstwo'] = $val[14];
+                $asddUp['cityc'] = $val[54];
                 $asddUp['city'] = ucwords($valb);
                 $asddUp['state'] = $val[9];
-                $asddUp['pincode'] = $val[7];
-                $asddUp['company_name'] = strtoupper(trim($val[4]));
-                $asddUp['gstin'] =  $val[30];
-                $asddUp['cus_code'] =  $val[0];
-                $asddUp['country'] =  $val[1];
-                $asddUp['zone'] =  $val[9];
-
-
+                $asddUp['pincode'] = $val[8];
+                $asddUp['company_name'] = strtoupper(trim($val[5]));
+                $asddUp['gstin'] =  $val[203];
+                $asddUp['cus_code'] =  $val[1];
+                $asddUp['country'] =  $val[2];
                 $add = Address::where('cus_code',$val[1])->where('user_id',$check_user->id)->update($asddUp);
 
                   // dd($add);
